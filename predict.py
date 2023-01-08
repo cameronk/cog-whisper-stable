a6b3da8757bfa01471b9d9003da902fdc06c031f
Cameron Kelley <cameron@kelley.vc>
2023-01-08 13:08:54 -0600

    Add base cog.yaml and predictor

---
+++ predict.py
@@ -0,0 +1,32 @@
+from typing import Any, List 
+
+import numpy as np
+from cog import BasePredictor, Input, Path, BaseModel
+from stable_whisper import load_model
+import whisper
+
+class Predictor(BasePredictor):
+    def setup(self):
+        self.models = {}
+        pass
+
+    def get_model(self, model: str) -> Any: 
+        if not model in whisper.available_models():
+            raise Exception("Model %s not found")
+        elif not model in self.models:
+            self.models[model] = load_model(model) # from stable-ts
+        return self.models[model]
+
+    def predict(
+        self,
+        audio: Path = Input(description="Audio to transcribe"),
+        model: str = Input(description="Whisper model to use", choices=whisper.available_models()),
+        stabilize_timestamps: bool = Input(description="Stabilize timestamps", default=True),
+    ) -> Any:
+        _whisper = self.get_model(model)
+        result = _whisper.transcribe(audio) # segments, transcription, detected_language
+        
+        if stabilize_timestamps:
+            result['segments'] = stabilize_timestamps(result, top_focus=True)
+        
+        return result
